08be1c845340d8a3bbc774724b12af1dce959c9d
rubin-khadka <rubin.robotic@gmail.com>
2026-04-02 15:26:54 +0200

    change in readmeip config, add different image and update code for modularity

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -65,9 +65,9 @@ uint8_t buffer[1000];
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -99,7 +99,7 @@ int main(void)
 
   USART1_Init();
 
-  if (W5500_Init() != 0)
+  if(W5500_Init() != 0)
   {
     Error_Handler();
   }
@@ -107,7 +107,7 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+  while(1)
   {
     /* USER CODE END WHILE */
 
@@ -119,17 +119,17 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -137,31 +137,30 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
 }
 
 /**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief SPI1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_SPI1_Init(void)
 {
 
@@ -185,7 +184,7 @@ static void MX_SPI1_Init(void)
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
   hspi1.Init.CRCPolynomial = 10;
-  if (HAL_SPI_Init(&hspi1) != HAL_OK)
+  if(HAL_SPI_Init(&hspi1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -196,10 +195,10 @@ static void MX_SPI1_Init(void)
 }
 
 /**
-  * @brief USART1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART1_UART_Init(void)
 {
 
@@ -218,7 +217,7 @@ static void MX_USART1_UART_Init(void)
   huart1.Init.Mode = UART_MODE_TX_RX;
   huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-  if (HAL_UART_Init(&huart1) != HAL_OK)
+  if(HAL_UART_Init(&huart1) != HAL_OK)
   {
     Error_Handler();
   }
@@ -229,10 +228,10 @@ static void MX_USART1_UART_Init(void)
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -246,10 +245,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, CS_Pin|RESET_Pin, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOB, CS_Pin | RESET_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pins : CS_Pin RESET_Pin */
-  GPIO_InitStruct.Pin = CS_Pin|RESET_Pin;
+  GPIO_InitStruct.Pin = CS_Pin | RESET_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
@@ -265,15 +264,15 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
+  while(1)
   {
   }
   /* USER CODE END Error_Handler_Debug */
--- Drivers/Ethernet_W5500/wizchip_port.c
+++ Drivers/Ethernet_W5500/wizchip_port.c
@@ -5,23 +5,26 @@
  *      Author: Rubin Khadka
  */
 
-
 #include "main.h"
 #include "wizchip_conf.h"
 #include "string.h"
 #include "socket.h"
 #include "usart1.h"
 
+// W5500 Initialization Return Codes
+typedef enum
+{
+  W5500_OK = 0,
+  W5500_ERR_INIT = -1,
+  W5500_ERR_COMM = -2,
+  W5500_ERR_LINK_DOWN = 3
+} W5500_Status_t;
+
 #define W5500_SPI hspi1
 
-wiz_NetInfo netInfo = {
-    .mac = {0x02, 0x00, 0x00, 0x00, 0x00, 0x01},
-    .ip = {192, 168, 1, 10},
-    .sn = {255, 255, 255, 0},
-    .gw = {0, 0, 0, 0},
-    .dns = {0, 0, 0, 0},
-    .dhcp = NETINFO_STATIC
-};
+wiz_NetInfo netInfo =
+    {.mac = {0x02, 0x00, 0x00, 0x00, 0x00, 0x01}, .ip = {192, 168, 1, 10}, .sn = {255, 255, 255, 0}, .gw = {0, 0, 0, 0}, .dns =
+        {0, 0, 0, 0}, .dhcp = NETINFO_STATIC};
 
 #define W5500_CS_LOW()     HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_RESET)
 #define W5500_CS_HIGH()    HAL_GPIO_WritePin(CS_GPIO_Port, CS_Pin, GPIO_PIN_SET)
@@ -31,105 +34,132 @@ wiz_NetInfo netInfo = {
 extern SPI_HandleTypeDef W5500_SPI;
 
 // SPI transmit/receive
-void W5500_Select(void)   { W5500_CS_LOW(); }
-void W5500_Unselect(void) { W5500_CS_HIGH(); }
+void W5500_Select(void)
+{
+  W5500_CS_LOW();
+}
+void W5500_Unselect(void)
+{
+  W5500_CS_HIGH();
+}
 
 uint8_t W5500_ReadByte(void)
 {
-    uint8_t rx;
-    uint8_t tx = 0xFF;
-    HAL_SPI_TransmitReceive(&W5500_SPI, &tx, &rx, 1, HAL_MAX_DELAY);
-    return rx;
+  uint8_t rx;
+  uint8_t tx = 0xFF;
+  HAL_SPI_TransmitReceive(&W5500_SPI, &tx, &rx, 1, HAL_MAX_DELAY);
+  return rx;
 }
 
 void W5500_WriteByte(uint8_t byte)
 {
-    HAL_SPI_Transmit(&W5500_SPI, &byte, 1, HAL_MAX_DELAY);
+  HAL_SPI_Transmit(&W5500_SPI, &byte, 1, HAL_MAX_DELAY);
 }
 
 int W5500_Init(void)
 {
-    uint8_t memsize[2][8] = {{2,2,2,2,2,2,2,2},{2,2,2,2,2,2,2,2}};
+  uint8_t memsize[2][8] = { {2, 2, 2, 2, 2, 2, 2, 2}, {2, 2, 2, 2, 2, 2, 2, 2}};
 
-    /***** Reset Sequence  *****/
-    W5500_RST_LOW();
-    HAL_Delay(50);
-    W5500_RST_HIGH();
-    HAL_Delay(200);
+  /***** Reset Sequence  *****/
+  W5500_RST_LOW();
+  HAL_Delay(50);
+  W5500_RST_HIGH();
+  HAL_Delay(200);
 
-    /***** Register callback  *****/
-    reg_wizchip_cs_cbfunc(W5500_Select, W5500_Unselect);
-    reg_wizchip_spi_cbfunc(W5500_ReadByte, W5500_WriteByte);
+  /***** Register callback  *****/
+  reg_wizchip_cs_cbfunc(W5500_Select, W5500_Unselect);
+  reg_wizchip_spi_cbfunc(W5500_ReadByte, W5500_WriteByte);
 
-    /***** Initialize the chip  *****/
-    if (ctlwizchip(CW_INIT_WIZCHIP, (void*)memsize) == -1) {
-        USART1_SendString("Error while initializing WIZCHIP\r\n");
-        return -1;
+  /***** Initialize the chip  *****/
+  if(ctlwizchip(CW_INIT_WIZCHIP, (void*) memsize) == -1)
+  {
+    USART1_SendString("Error while initializing WIZCHIP\r\n");
+    return W5500_ERR_INIT;
+  }
+  USART1_SendString("WIZCHIP Initialized\r\n");
+
+  /***** Check communication by reading Version  *****/
+  uint8_t ver = getVERSIONR();
+  if(ver != 0x04)
+  {
+    USART1_SendString("Error Communicating with W5500 Version: ");
+    USART1_SendHex(ver);
+    USART1_SendString("\r\n");
+    return W5500_ERR_COMM;
+  }
+  USART1_SendString("Checking Link Status..\r\n");
+
+  /***** Check Link Status  *****/
+  uint8_t link = PHY_LINK_OFF;
+  uint8_t retries = 10;
+  while((link != PHY_LINK_ON) && (retries > 0))
+  {
+    ctlwizchip(CW_GET_PHYLINK, &link);
+    if(link == PHY_LINK_ON)
+      USART1_SendString("Link: UP\r\n");
+    else
+    {
+      USART1_SendString("Link: DOWN Retrying: ");
+      USART1_SendNumber(10 - retries);
+      USART1_SendString("\r\n");
     }
-    USART1_SendString("WIZCHIP Initialized\r\n");
+    retries--;
+    HAL_Delay(500);
+  }
+  if(link != PHY_LINK_ON)
+  {
+    USART1_SendString("Link is Down, please reconnect and retry\r\n");
+    USART1_SendString("Exiting Setup..\r\n");
+    return W5500_ERR_LINK_DOWN;
+  }
 
-    /***** Check communication by reading Version  *****/
-    uint8_t ver = getVERSIONR();
-    if (ver != 0x04) {
-        USART1_SendString("Error Communicating with W5500 Version: ");
-        USART1_SendHex(ver);
-        USART1_SendString("\r\n");
-        return -2;
-    }
-    USART1_SendString("Checking Link Status..\r\n");
+  /***** Configure Static IP  *****/
+  USART1_SendString("Using Static IP..\r\n");
+  ctlnetwork(CN_SET_NETINFO, (void*) &netInfo);
 
-    /***** Check Link Status  *****/
-    uint8_t link = PHY_LINK_OFF;
-    uint8_t retries = 10;
-    while ((link != PHY_LINK_ON) && (retries > 0)) {
-        ctlwizchip(CW_GET_PHYLINK, &link);
-        if (link == PHY_LINK_ON)
-            USART1_SendString("Link: UP\r\n");
-        else {
-            USART1_SendString("Link: DOWN Retrying: ");
-            USART1_SendNumber(10-retries);
-            USART1_SendString("\r\n");
-        }
-        retries--;
-        HAL_Delay(500);
-    }
-    if (link != PHY_LINK_ON) {
-        USART1_SendString("Link is Down, please reconnect and retry\r\n");
-        USART1_SendString("Exiting Setup..\r\n");
-        return 3;
-    }
+  /***** Print assigned IP on the console  *****/
+  wiz_NetInfo tmpInfo;
+  ctlnetwork(CN_GET_NETINFO, &tmpInfo);
 
-    /***** Configure Static IP  *****/
-    USART1_SendString("Using Static IP..\r\n");
-    ctlnetwork(CN_SET_NETINFO, (void*)&netInfo);
+  USART1_SendString("IP: ");
+  USART1_SendNumber(tmpInfo.ip[0]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.ip[1]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.ip[2]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.ip[3]);
+  USART1_SendString("\r\n");
 
-    /***** Print assigned IP on the console  *****/
-    wiz_NetInfo tmpInfo;
-    ctlnetwork(CN_GET_NETINFO, &tmpInfo);
+  USART1_SendString("SUBNET: ");
+  USART1_SendNumber(tmpInfo.sn[0]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.sn[1]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.sn[2]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.sn[3]);
+  USART1_SendString("\r\n");
 
-    USART1_SendString("IP: ");
-    USART1_SendNumber(tmpInfo.ip[0]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.ip[1]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.ip[2]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.ip[3]); USART1_SendString("\r\n");
+  USART1_SendString("GATEWAY: ");
+  USART1_SendNumber(tmpInfo.gw[0]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.gw[1]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.gw[2]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.gw[3]);
+  USART1_SendString("\r\n");
 
-    USART1_SendString("SUBNET: ");
-    USART1_SendNumber(tmpInfo.sn[0]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.sn[1]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.sn[2]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.sn[3]); USART1_SendString("\r\n");
+  USART1_SendString("DNS: ");
+  USART1_SendNumber(tmpInfo.dns[0]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.dns[1]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.dns[2]);
+  USART1_SendString(".");
+  USART1_SendNumber(tmpInfo.dns[3]);
+  USART1_SendString("\r\n");
 
-    USART1_SendString("GATEWAY: ");
-    USART1_SendNumber(tmpInfo.gw[0]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.gw[1]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.gw[2]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.gw[3]); USART1_SendString("\r\n");
-
-    USART1_SendString("DNS: ");
-    USART1_SendNumber(tmpInfo.dns[0]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.dns[1]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.dns[2]); USART1_SendString(".");
-    USART1_SendNumber(tmpInfo.dns[3]); USART1_SendString("\r\n");
-
-    return 0;
+  return W5500_OK;
 }
